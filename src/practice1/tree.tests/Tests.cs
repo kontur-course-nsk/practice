d3ec77070f2d8b00452ef014ca8e523a78e84877
a_mastrakov <a_mastrakov@kontur.ru>
2018-10-09 18:59:55 +0700

    init repo with task for first practice lesson

---
+++ src/practice1/tree.tests/Tests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace Tree.Tests
+{
+    public class Tests
+    {
+        public const string FullResult = @"├───project
+│	├───file.txt (9b)
+│	└───kontur.png (6796b)
+├───static
+│	├───a_lorem
+│	│	├───dolor.txt (empty)
+│	│	├───ipsum
+│	│	│	└───kontur.png (6796b)
+│   │   └───kontur.png (6796b)
+│	├───css
+│	│	└───body.css (28b)
+│	├───empty.txt (empty)
+│	├───html
+│	│	└───index.html (57b)
+│	├───js
+│	│	└───site.js (10b)
+│	└───z_lorem
+│		├───dolor.txt (empty)
+│		├───kontur.png (6796b)
+│		└───z_ipsum
+│			└───kontur.png (6796b)
+├───zline
+│	├───empty.txt (empty)
+│	└───lorem
+│		├───dolor.txt (empty)
+│		└───kontur.png (6796b)
+└───zzfile.txt (empty)";
+
+        public const string DirectoriesOnlyResult = @"├───project
+├───static
+│	├───a_lorem
+│	│	└───ipsum
+│	├───css
+│	├───html
+│	├───js
+│	└───z_lorem
+│		└───z_ipsum
+└───zline
+	└───lorem";
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            Environment.CurrentDirectory = Path.GetDirectoryName(typeof(Tests).Assembly.Location);
+        }
+
+        [Test]
+        public void FullResultTest()
+        {
+            var writer = new StringWriter();
+            Program.PrintDirectoryTree("testData", true, writer);
+
+            Assert.AreEqual(FullResult, writer.ToString());
+        }
+
+        [Test]
+        public void DirectoriesOnlyResultTest()
+        {
+            var writer = new StringWriter();
+            Program.PrintDirectoryTree("testData", false, writer);
+
+            Assert.AreEqual(DirectoriesOnlyResult, writer.ToString());
+        }
+    }
+}
---
+++ src/practice1/tree/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Tree
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 1 && args.Length != 2)
+            {
+                Console.WriteLine("Usage tree.exe {path} [-f]");
+                return;
+            }
+
+            var path = args[0];
+            var printFiles = args.Length == 2 && args[1] == "-f";
+            PrintDirectoryTree(path, printFiles, Console.Out);
+        }
+
+        public static void PrintDirectoryTree(string path, bool printFiles, TextWriter writer)
+        {
+            // todo: реализовать этот метод. 
+            // Результат записывать как writer.Write() или writer.WriteLine()
+
+        }
+    }
+}
