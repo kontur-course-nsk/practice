56c37a078e0ec1e018e346f7a92d79ffbc495989
Backs <rogatnev.sergey@gmail.com>
2018-12-04 01:26:25 +0700

    - начало

---
+++ Parser.Generator/DataGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Parser.Generator
+{
+    public static class DataGenerator
+    {
+        public static Stream Generate(int count)
+        {
+            return EnumerableStream.Create(GetData(count), t => Encoding.UTF8.GetBytes(t));
+        }
+
+        private static Random rand = new Random(DateTime.Now.Ticks.GetHashCode());
+
+        private static readonly string[] names = { "Mark", "David", "Jhon", "Freddie", "Marshall", "Jessica", "Kate" };
+
+        private static IEnumerable<string> GetData(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return $"{names[rand.Next(names.Length)]} Age: {rand.Next(18, 43)}" + Environment.NewLine;
+                yield return $"--- Amount: {rand.Next(100, 1000) * 10}" + Environment.NewLine;
+            }
+        }
+
+    }
+}
---
+++ Parser.Generator/EnumerableStream.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Parser.Generator
+{
+    public static class EnumerableStream
+    {
+        public static EnumerableStream<T> Create<T>(IEnumerable<T> source, Func<T, byte[]> serializer)
+        {
+            return new EnumerableStream<T>(source, serializer);
+        }
+    }
+
+    public class EnumerableStream<T> : Stream
+    {
+        private readonly IEnumerator<T> _source;
+        private readonly Func<T, IEnumerable<byte>> _serializer;
+        private readonly Queue<byte> _buf = new Queue<byte>();
+
+        public override bool CanRead => true;
+        public override bool CanSeek => false;
+        public override bool CanWrite => false;
+        public override long Length => -1;
+
+        public EnumerableStream(IEnumerable<T> source, Func<T, IEnumerable<byte>> serializer)
+        {
+            _source = source.GetEnumerator();
+            _serializer = serializer;
+        }
+
+        private bool SerializeNext()
+        {
+            if (!_source.MoveNext())
+                return false;
+
+            foreach (var b in _serializer(_source.Current))
+                _buf.Enqueue(b);
+
+            return true;
+        }
+
+        private byte? NextByte()
+        {
+            if (_buf.Any() || SerializeNext())
+            {
+                return _buf.Dequeue();
+            }
+
+            return null;
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            var read = 0;
+            while (read < count)
+            {
+                var mayb = NextByte();
+                if (mayb == null) break;
+
+                buffer[offset + read] = (byte)mayb;
+                read++;
+            }
+
+            return read;
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void Flush()
+        {
+            throw new NotSupportedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override long Position
+        {
+            get => throw new NotSupportedException();
+            set => throw new NotSupportedException();
+        }
+    }
+}
