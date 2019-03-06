bacc68b77c139dd8ed3bddacb7d819a8a8d8658c
mastrakov <avmastrakov@gmail.com>
2019-03-06 11:43:40 +0700

    add assignment for async programming practice

---
+++ src/practice13/AsyncAssignment/MainWorker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Async.External.Lib;
+
+namespace AsyncAssignment
+{
+    public class MainWorker
+    {
+        private readonly AsyncWordCounter counter = new AsyncWordCounter();
+        
+        public string[] SplitToWordsAndCount(string input)
+        {
+            var words = AsyncWorker.SplitAsync(input).Result;
+            counter.AddWordsAsync(words).Wait();
+            return words;
+        }
+
+        public IEnumerable<KeyValuePair<string, int>> GetTop10АrequentlyUsedWords()
+        {
+            return counter.GetTop10АrequentlyUsedWords();
+        }
+    }
+}
